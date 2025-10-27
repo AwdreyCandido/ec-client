fbfc7254e44bec8ebb83956f8b0a5027955c4f57
Awdrey <awdrey@hippo-tech.com>
2025-10-27 13:41:57 -0300

    fix loading spinner

--- src/components/custom/loading/PageLoading.tsx
+++ src/components/custom/loading/PageLoading.tsx
@@ -2,17 +2,18 @@ import classes from "./PageLoading.module.css";
 
 const PageLoading = () => {
   return (
-    <>
-      <div className="absolute w-screen h-screen left-0 top-0 bg-[#ccc]/10 backdrop-blur-xs z-[1100]"></div>
-      <div className="absolute flex justify-center items-center w-full h-screen z-[1100]">
-        <div className="bg-secondary-light p-4 shadow-lg rounded-full relative">
+    <div className="fixed inset-0 z-[1100] flex items-center justify-center">
+      <div className="absolute inset-0 bg-[#ccc]/10 backdrop-blur-sm"></div>
+
+      <div className="relative z-[1101] flex items-center justify-center">
+        <div className="bg-secondary-light p-4 shadow-lg rounded-full">
           <div className={classes.circle_spinner}>
             <div className={classes.circle}></div>
             <div className={classes.orbit}></div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
